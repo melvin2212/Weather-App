d98711694023be904ff54d2750c3f80922c60c55
melvin2212 <44001055+melvin2212@users.noreply.github.com>
2018-12-09 15:47:23 -0500

    Update program.py

--- program.py
+++ program.py
@@ -1,70 +1,39 @@
-# This python program is written to provide weather details of any city using openweather api
 
-# import required modules
 import requests, json
 
-# Api key
 api_key = "6b2d7c489fcdb0e84d27fd48ce4b8c0b"
-
-# base_url variable to store url
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-# Enter city name
+
 city_name = input("Enter city name : ")
 
-# complete_url variable to store
-# complete url address
+
 complete_url = base_url + "appid=" + api_key + "&q=" + city_name
 
-# get method of requests module
-# return response object
+
 response = requests.get(complete_url)
 
-# json method of response object
-# convert json format data into
-# python format data
+
 x = response.json()
 
-# Now x contains list of nested dictionaries
-# Check the value of "cod" key is equal to
-# "404", means city is found otherwise,
-# city is not found
 if x["cod"] != "404":
 
-    # store the value of "main"
-    # key in variable y
     y = x["main"]
 
-    # store the value corresponding
-    # to the "temp" key of y
+   
     current_temperature = y["temp"]
-
-    # store the value corresponding
-    # to the "pressure" key of y
-    current_pressure = y["pressure"]
-
-    # store the value corresponding
-    # to the "humidity" key of y
     current_humidiy = y["humidity"]
 
-    # store the value of "weather"
-    # key in variable z
     z = x["weather"]
 
-    # store the value corresponding
-    # to the "description" key at
-    # the 0th index of z
     weather_description = z[0]["description"]
 
-    # this is the results
-    print(" Temperature (in kelvin unit) = " +
+    print(" Temperature (kelvin) =  " +
                     str(current_temperature) +
-          "\n atmospheric pressure (in hPa unit) = " +
-                    str(current_pressure) +
           "\n humidity (in percentage) = " +
                     str(current_humidiy) +
           "\n description = " +
                     str(weather_description))
 
 else:
-    print(" City Not Found ")
+    print(" This city was not found ")
